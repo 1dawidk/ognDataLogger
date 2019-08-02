8fc77a2baa5e2dfa5f89501d7b1c800565573b1d
1dawidk <1dawidk@gmail.com>
2019-08-02 20:55:33 +0200

    return string as double!!?? bug fix

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# Exit if any subprogram exits with an error
+set -e
+
 echo "Install required dependencies"
 apt-get update
-apt install libeigen3-dev libpng++-dev libboost-all-dev git cmake
+apt install libeigen3-dev libpng++-dev libboost-all-dev libpng-dev git cmake
 
 #Build misc library
 echo "Build ogn library..."
+
+#cd ./cpp-lib/
+#./scripts/build.sh
 
 cd ./cpp-lib/prj
 ln -s def.compiler.gcc-5-ubuntu-14 def.compiler
 ln -s def.platform.linux def.platform
 make
+
+
 
 cd ../..
 mkdir ./build
