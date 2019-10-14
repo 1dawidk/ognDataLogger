94fa6badd1d9a6973fda9935fae11aef74d9af54
1dawidk <1dawidk@gmail.com>
2019-10-14 13:46:32 +0200

    Watchdog test: step 3

--- Watchdog.cpp
+++ Watchdog.cpp
@@ -16,7 +16,7 @@ void Watchdog::onStart() {
 void Watchdog::onRun() {
     if(ognInstance->getLastKeepaliveTime()+2<Clock::GetDayMinutes()){
         debugLog->write("Watchdog", "OGN connection is DEAD. Trying to reconnect");
-        //ognInstance->resetConnection();
+        ognInstance->resetConnection();
         Thread::pause(120000); // Wait 5min for connection to establish
     }
     Thread::pause(1000);
