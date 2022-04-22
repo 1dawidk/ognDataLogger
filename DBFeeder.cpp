12b0e2e1be51cbd82d9825b1fba54a4b62f0956b
1dawidk <1dawidk@gmail.com>
2022-04-22 19:58:36 +0200

    Backup added

--- DBFeeder.cpp
+++ DBFeeder.cpp
@@ -24,18 +24,22 @@ void DBFeeder::onRun() {
         if( fileLines.size() ) {
 
             ofstream sendFile;
+            ofstream backupFile;
             sendFile.open(dataDir + "sendLog.data");
+            backupFile.open(dataDir+"ognData.backup", ios::app);
             for (int i = 0; i < fileLines.size(); i++) {
                 sendFile << fileLines[i];
+                backupFile << fileLines[i] << endl;
                 if (i != fileLines.size() - 1)
                     sendFile << endl;
             }
             sendFile.close();
-
+            backupFile.close();
 
             CURL *curlHandle = curl_easy_init();
             curl_httppost *post = NULL;
             curl_httppost *lastpost = NULL;
+            std::string readBuffer;
 
             // Create http header
             struct curl_slist *headerlist = NULL;
@@ -56,6 +60,10 @@ void DBFeeder::onRun() {
 
             // Perform curl
             curl_easy_perform(curlHandle);
+
+            long response_code;
+            curl_easy_getinfo(curlHandle, CURLINFO_RESPONSE_CODE, &response_code);
+
             curl_easy_cleanup(curlHandle);
 
             if(now) {
@@ -65,8 +73,13 @@ void DBFeeder::onRun() {
             }
 
             char msg[256];
-            sprintf(msg, "Sent to server [ %d lines today ]", linesToday);
-            debugLog->write("DBFeeder", msg);
+            if(response_code==200){
+                sprintf(msg, "Sent to server [ %d lines today ]", linesToday);
+                debugLog->write("DBFeeder", msg);
+            } else {
+                sprintf(msg, "Server error! Responded with %ld", response_code);
+                debugLog->write("DBFeeder", msg);
+            }
         } else {
             char msg[256];
             sprintf(msg, "Nothing to log [ %d lines today ]", linesToday);
